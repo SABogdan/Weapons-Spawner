b040a2e4977595be92e144c7e872e0e0d70613e2
Bogdan <purdabogdanalexandru@gmail.com>
2020-05-03 11:03:42 +0300

    $_Bug: Added the .cs

---
+++ Main.cs
@@ -0,0 +1,30 @@
+﻿
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class WeaponSpawner : BaseScript
+    {
+        public WeaponSpawner()
+        {
+            API.RegisterCommand("guns", new Action<int, List<object>, string>((src, args, raw) =>
+            { 
+                foreach (WeaponHash weapon in Enum.GetValues(typeof(WeaponHash)))
+                {
+                    if (Game.PlayerPed.Weapons.HasWeapon(weapon))
+                    {
+                        Game.PlayerPed.Weapons.Remove(weapon);
+                    }
+                    else 
+                    { 
+                        Game.PlayerPed.Weapons.Give(weapon, 250, false, true);
+                    }
+                }
+            }), false);
+        }
+    }
+}
